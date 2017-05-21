68351040260f64ffbc2ddfcd8c334f656d6a5020
Andrew <andrew.valencik@gmail.com>
2017-05-21 12:04:20 -0300

    Adds update/upload script

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python yaml-resume-to-json.py \
+  && resume export web/index.html -t stackoverflow \
+  && resume export web/short.html -t short \
+  && resume export web/crisp.html -t crisp \
+  && scp web/* cs:~/public_html/resume/
